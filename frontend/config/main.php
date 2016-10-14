81063403bd2abd72ad5793fdf009fdb15cde4e49
feifei <phphome@qq.com>
2016-10-15 02:26:22 +0800

    init ...

---
+++ frontend/config/main.php
@@ -0,0 +1,50 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-home',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    /* 默认路由 */
+    'defaultRoute' => 'index',
+    /* 控制器默认命名空间 */
+    'controllerNamespace' => 'frontend\controllers',
+
+    'components' => [
+        'user' => [
+            'identityClass' => 'frontend\models\User',
+            'enableAutoLogin' => true,
+        ],
+        /* 修改默认的request组件 */
+        'request' => [
+            'class' => 'common\core\Request',
+            'baseUrl' => '',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'public/404',
+        ],
+
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+
+    ],
+    'params' => $params,
+];
---
+++ frontend/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\web\Controller;
+
+class IndexController extends Controller
+{
+    public function actionIndex()
+    {
+        phpinfo();
+        return $this->render('index');
+    }
+}
